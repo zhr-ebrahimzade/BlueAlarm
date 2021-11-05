4591adb9ae8d0205c480bd4483907460bc992255
Zahra Ebrahim zade <eb.zahra99@gamil.com>
2021-11-06 01:20:54 +0330

    Sign Up activity completion(1/2)

--- app/src/main/java/com/alarm/bluealarm/SignUp.java
+++ app/src/main/java/com/alarm/bluealarm/SignUp.java
@@ -2,7 +2,10 @@ package com.alarm.bluealarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 public class SignUp extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+     /*   WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.x = -20;
+        params.height = 100;
+        params.width = 550;
+        params.y = -10;
+
+        this.getWindow().setAttributes(params);*/
     }
 }
