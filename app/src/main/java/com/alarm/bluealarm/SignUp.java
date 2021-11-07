1130ac333c9158680e2f8a172df2254f11a080e3
Zahra Ebrahim zade <eb.zahra99@gamil.com>
2021-11-07 13:27:08 +0330

    sign uo java class (part 1)

--- app/src/main/java/com/alarm/bluealarm/SignUp.java
+++ app/src/main/java/com/alarm/bluealarm/SignUp.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class SignUp extends AppCompatActivity /*implements View.OnClickListener*/ {
+public class SignUp extends AppCompatActivity implements View.OnClickListener{
     private static final String TAG="SignUp";
     EditText name, user,email, pass, confirm;
 
@@ -18,11 +18,11 @@ public class SignUp extends AppCompatActivity /*implements View.OnClickListener*
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        //init();
+        init();
 
-        //sign_btn.setOnClickListener(this);
+        sign_btn.setOnClickListener(this);
 
-    } /*
+    }
     @Override
     public void onClick(View v) {
 
@@ -33,15 +33,15 @@ public class SignUp extends AppCompatActivity /*implements View.OnClickListener*
     }
 
     private void init(){
-        name.findViewById(R.id.fullName);
-        user.findViewById(R.id.username);
-        email.findViewById(R.id.email);
-        pass.findViewById(R.id.password);
-        confirm.findViewById(R.id.confirmPass);
-        sign_btn.findViewById(R.id.signUp_btn);
+        name=findViewById(R.id.fullName);
+        user=findViewById(R.id.username);
+        email=findViewById(R.id.email);
+        pass=findViewById(R.id.password);
+        confirm=findViewById(R.id.confirmPass);
+        sign_btn=findViewById(R.id.signUp_btn);
     }
 
-    *//*checks every view to not be empty*//*
+    /*checks every view to not be empty*/
     private boolean check(){
         boolean isOk=true;
         if (name.getText().toString().trim().isEmpty()
@@ -55,29 +55,29 @@ public class SignUp extends AppCompatActivity /*implements View.OnClickListener*
         }
         else {//returns the number of characters
             if (name.getText().toString().trim().length()<2) {
-                Toast.makeText(this, "Not enough char", Toast.LENGTH_SHORT);
+                Toast.makeText(this, "Not enough char", Toast.LENGTH_SHORT).show();
                 isOk=false;
             }
             if (user.getText().toString().trim().length()<2) {
-                Toast.makeText(this, "Not enough char", Toast.LENGTH_SHORT);
+                Toast.makeText(this, "Not enough char", Toast.LENGTH_SHORT).show();
                 isOk=false;
             }
             if (email.getText().toString().trim().length()<12){
-                Toast.makeText(this,"",Toast.LENGTH_SHORT);
+                Toast.makeText(this,"",Toast.LENGTH_SHORT).show();
                 isOk=false;
             }
             else {
                 if (!email.getText().toString().contains("@")) {
-                    Toast.makeText(this, "Invalid email", Toast.LENGTH_SHORT);
+                    Toast.makeText(this, "Invalid email", Toast.LENGTH_SHORT).show();
                     isOk=false;
                 }
             }
             if (pass.getText().toString().trim().length()<8) {
-                Toast.makeText(this, "Not enough char", Toast.LENGTH_SHORT);
+                Toast.makeText(this, "Not enough char", Toast.LENGTH_SHORT).show();
                 isOk=false;
             }
-            if (confirm==pass){
-                Toast.makeText(this,"Not matched",Toast.LENGTH_SHORT);
+            if (confirm.getText().toString().equals(pass.getText().toString())){
+                Toast.makeText(this,"Not matched",Toast.LENGTH_SHORT).show();
                 isOk=false;
             }
 
@@ -85,6 +85,6 @@ public class SignUp extends AppCompatActivity /*implements View.OnClickListener*
         return isOk;
     }
 
-*/
+
 
 }
