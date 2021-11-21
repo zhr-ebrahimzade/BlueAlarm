49c3de18e758dc79bd80ee859c16eae9d71ecfc8
Arezoo <arezookmn1@gmail.com>
2021-11-21 15:36:54 +0330

    Add Navigation Drawer to MainActivity

--- app/src/main/java/com/alarm/bluealarm/MainActivity.java
+++ app/src/main/java/com/alarm/bluealarm/MainActivity.java
@@ -1,41 +1,104 @@
 package com.alarm.bluealarm;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
-    private Button logoutBtn;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    //variables
+    private MenuItem logoutBtn;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    FirebaseAuth firebaseAuth ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-        init();
 
-        logoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(MainActivity.this, Welcome.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        firebaseAuth = FirebaseAuth.getInstance();
+        //Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_main);
+        toolbar = findViewById(R.id.toolbar);
+
+        //Toolbar
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //Navigation Drawer Menu
+        navigationView.bringToFront();
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_close, R.string.nav_open);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.homeItem);
+
+        // get logout btn
+        Menu menu = navigationView.getMenu();
+        logoutBtn = menu.findItem(R.id.logoutItem);
 
     }
-    //testing
-    //test 2
 
-    public void init(){
-       // logoutBtn = findViewById(R.id.logoutBtn);
-    */
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+        super.onBackPressed();
+        }
     }
 
 
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.homeItem:
+                break;
+            case R.id.settingItem:
+                break;
+            case R.id.shareItem:
+                Toast.makeText(this, "Share" , Toast.LENGTH_LONG).show();
+                break;
+            case R.id.rateItem:
+                break;
+            case R.id.logoutItem:
+                logoutBtn.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        firebaseAuth.signOut();
+                        Intent intent = new Intent(MainActivity.this, Welcome.class);
+                        startActivity(intent);
+                        finish();
+                        return true;
+                    }
+                });
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
+
+
